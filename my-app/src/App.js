386077c08be8cbc298568cc4bb7348f5ca31f6b4
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-11 15:20:12 +0000

    Initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
-     <FunctionalGreeting />
-     <FunctionalGreetingWithProps />
+     <StatefulGreetingWithCallback/>
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class StatefulGreetingWithCallback extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    handleClick() {
+        this.setState({
+            introduction: 'Goodbye!',
+            buttonText: 'Enter'
+        });
+        console.log(this.state.introduction);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+
+        )
+    }
+}
+
+export default StatefulGreetingWithCallback;
