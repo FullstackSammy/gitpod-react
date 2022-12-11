fe788955d445247c420a4d66536954ff0b0661e3
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-11 19:17:50 +0000

    added cheatsheet for ES7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
